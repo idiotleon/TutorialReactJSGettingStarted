fbaeea6fcd5e3bca052f9d5d74665c10f984dfdc
Yang Lu <leon@Yangs-MBP.home>
2018-04-04 13:25:16 -0700

    demo example: change state of a component

--- Component/App.jsx
+++ Component/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
-    state = {
-        counter: 0
-    }
+    state = { counter: 0 };
+
+    handleClick = () => {
+        // "this"[keyword] refers to the component instance
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    };
+
     render() {
         return (
-            <button>{this.state.counter}</button>
+            <button onClick={this.handleClick}>
+                {this.state.counter}
+            </button>
         );
     }
 }
