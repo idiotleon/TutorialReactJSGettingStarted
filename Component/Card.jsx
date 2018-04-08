b36d6b80c9308646c61c722db1a137ecdcaa8af3
Yang Lu <yanglyu.leon.7@gmail.com>
2018-04-08 10:27:42 -0700

    demo example: pass a list of data via props
    a live demo of the spread operator
    
    Module 3.1 Build a Github Card Component

--- Component/Card.jsx
+++ Component/Card.jsx
@@ -8,16 +8,14 @@ const Card = (props) => {
                 <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}> {props.name}</div>
                 <div>{props.company}</div>
             </div>
-        </div >
+        </div>
     );
 };
 
 const CardList = (props) => {
     return (
         <div>
-            <Card name="Paul O'Shannessy"
-                avatar_url="https://avatars1.githubusercontent.com/u/8445?v=4"
-                company="Facebook" />
+            {props.cards.map(card => <Card {...card} />)}
         </div>
     );
 }
--- main.js
+++ main.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './Component/App.jsx';
 import Card from './Component/Card.jsx';
 
-ReactDOM.render(<Card />, document.getElementById('app'));
+let data = [
+    {
+        name: "Paul O'Shannessy",
+        avatar_url: "https://avatars1.githubusercontent.com/u/8445?v=4",
+        company: "Facebook"
+    }, {
+        name: "Ben Alpert",
+        avatar_url: "https://avatars0.githubusercontent.com/u/7585659?v=4",
+        company: "Facebook"
+    }
+];
+
+ReactDOM.render(<Card cards={data} />, document.getElementById('app'));
