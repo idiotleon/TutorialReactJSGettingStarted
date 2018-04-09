20efd152b856e9bbb853d60cd937d571925dc1c3
Yang Lu <yanglyu.leon.7@gmail.com>
2018-04-08 20:40:48 -0700

    demo example: Setup for font-awesome

--- Component/Game.jsx
+++ Component/Game.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+require('font-awesome/css/font-awesome.min.css');
+
+const Stars = (props) => {
+    return (
+        <div>
+            <i className="fa fa-star" />
+            <i className="fa fa-star" />
+            <i className="fa fa-star" />
+            <i className="fa fa-star" />
+        </div>
+    );
+}
+
+const Button = (props) => {
+    return (
+        <div>
+            Btn
+        </div>
+    );
+}
+
+const Answer = (props) => {
+    return (
+        <div>
+            Answer
+        </div>
+    );
+}
 
 class Game extends React.Component {
     render() {
         return (
             <div>
                 <h3>Play Nine</h3>
+                <Stars />
+                <Button />
+                <Answer />
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,21 @@ var config = {
                 query: {
                     presets: ['es2015', 'react', 'stage-2']
                 }
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader"
             }
         ]
     }
 }
+
 module.exports = config;
