894fc8abc8ebea1829d77da5e4845e2bf36dff5f
Leon Lu <yanglu@microsoft.com>
2018-04-10 17:59:23 -0700

    demo example: display stars using a for loop and an array
    Module 4.3 Random Number of Stars

--- Component/Game.jsx
+++ Component/Game.jsx
@@ -4,12 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../CSS/main.css';
 
 const Stars = (props) => {
+    const numberOfStars = 5;
+
+    let stars = [];
+    for(let i = 0; i < numberOfStars; i++){
+        stars.push(<i key={i} className="fa fa-star"/>);
+    }
+
     return (
         <div class="col-5">
-            <i className="fa fa-star" />
-            <i className="fa fa-star" />
-            <i className="fa fa-star" />
-            <i className="fa fa-star" />
+            {stars}
         </div>
     );
 }
